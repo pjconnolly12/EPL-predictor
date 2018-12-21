1027358c9fa99a7731a287702b5ab91f4483223a
unknown <pjconnolly12@gmail.com>
2018-12-20 20:52:44 -0500

    trying to revise forms

--- project/selections/forms.py
+++ project/selections/forms.py
@@ -1,11 +1,8 @@
 # project/selections/forms.py
 
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import SelectField
 from wtforms.validators import DataRequired
 
 class SelectionForm(Form):
-	selection = StringField(
-		'selection',
-		validators=[DataRequired()]
-		)
+	selection = RadioField('Label')
